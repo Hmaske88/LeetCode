dc8800253fac2e095622af1efa630beb8801df9e
Hemant Maske <89325376+Hmaske88@users.noreply.github.com>
2023-08-03 19:18:16 +0530

    Update Number of Recent Calls.py

--- LeetCode 75/Queue/Number of Recent Calls.py
+++ LeetCode 75/Queue/Number of Recent Calls.py
@@ -12,3 +12,16 @@ class RecentCounter:
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
+
+
+# OR
+class RecentCounter:
+
+    def __init__(self):
+        self.queue=[]
+        
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        while self.queue[0]<t-3000:
+            self.queue.pop(0)
+        return len(self.queue)
