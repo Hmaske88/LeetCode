1be32dac135dcbaa1bf86a73902491b1aca0494f
Hemant Maske <89325376+Hmaske88@users.noreply.github.com>
2023-09-17 06:24:03 +0530

    Create 2103-Rings and Rods.py

---
+++ Problem List/2103-Rings and Rods.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        count=0
+        for i in range(10):
+            i=str(i)
+            if("R"+i in rings and "G"+i in rings and "B"+i in rings):
+                count+=1
+        return count
