1936eb2d9ab1cf1602860dedfd23990e22ec20ff
Hemant Maske <89325376+Hmaske88@users.noreply.github.com>
2024-01-11 09:48:03 +0530

    Create 2540-Minimum Common Value.py

---
+++ Problem List/2540-Minimum Common Value.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i=j=0
+        while(len(nums1)>i and len(nums2)>j): 
+            if(nums1[i]==nums2[j]):
+                return nums1[i]
+            if(nums1[i]<nums2[j]):
+                i+=1
+            else:
+                j+=1
+        return -1
